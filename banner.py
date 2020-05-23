2ed36d7641f507d58f131f9c29e8ff48303377f2
wedusganteng <63887769+wedusganteng@users.noreply.github.com>
2020-05-23 23:57:02 +0700

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,47 @@
+ban = """   
+               ___
+              | + | 
+          ____| + |___  Wa  : +6287837482552
+          |    O O    | Git : github.com/wedusganteng
+          |___  -   __| 
+              |   | 
+              |___| 
+          Instagram Tools
+[1] User Profile Information
+[2] Download Video Post
+[3] Download Image Post
+[4] Exit
+"""
+
+ban1 = """   
+               ___
+              | + | 
+          ____| + |___  Wa  : +6287837482552
+          |    O O    | Git : github.com/wedusganteng
+          |___  -   __| 
+              |   | 
+              |___| 
+      User Profile Information
+"""
+
+ban2 = """   
+               ___
+              | + | 
+          ____| + |___  Wa  : +6287837482552
+          |    O O    | Git : github.com/wedusganteng
+          |___  -   __| 
+              |   | 
+              |___| 
+         Video Downloader
+"""
+
+ban3 = """   
+               ___
+              | + | 
+          ____| + |___   Wa  : +6287837482552
+          |    O O    | Git : github.com/wedusganteng
+          |___  -   __| 
+              |   | 
+              |___| 
+         Image Downloader
+"""
